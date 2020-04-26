b62ede9693cde2435b5e07ed57bddca077a50fcc
Hamdi <benyaflahhamdi2@gmail.com>
2020-04-26 17:07:30 -0400

    broken our homepage into smaller component and finish syling our menu item component

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import './homepage.styles.scss';
+
 
 const HomePage = () => (
     <div className="homepage">
@@ -12,12 +14,6 @@ const HomePage = () => (
             <div className="menu-item">
                 <div className="content">
                     <h1 className="title">JACKETS</h1>
-                    <span className="subtitle">SHOP NOW</span>
-                </div>
-            </div>
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">HATS</h1>
                     <span className="subtitle">SHOP NOW</span>
                 </div>
             </div>
